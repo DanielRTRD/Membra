6f23f619d6d23cd395af3370f8eb099673819097
Daniel Billing <daniel@retardedtech.com>
2015-05-10 19:11:18 +0200

    Template changes

--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -1,35 +1,36 @@
-@extends('layout.main')
+@extends('layouts.base.main')
 @section('title', 'My Account')
 @section('content')
 
-<div class="row">
-	<div class="col-md-12">
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
 
-        <h2>My Account</h2>
-        <hr>
-		<div class="row">
-			<div class="col-lg-3">
-				@include('layouts.base.account-sidebar')
-			</div>
-			<div class="col-lg-9">
-				<h3>Welcome back, {{ $firstname }}!</h3>
-				<div class="row">
-					<div class="col-lg-6">
-						<p>Today is {!! $getUserDateFormat  !!}, and the time is {{ $getUserDateFormat  }}.</p>
-					</div>
-					<div class="col-lg-6">
-						<p>
-							<strong>Your referral link:</strong><br>
-							<input class="form-control" type="text" name="referrallink" id="referrallink" value="http://pkmn.cards/r/{{$user->referral_code}}">
-						</p>
-						<p>You have referred <strong></strong> user(s).</p>
-					</div>
+			<h2>My Account</h2>
+			<hr>
+			<div class="row">
+				<div class="col-lg-3">
+					@include('layouts.base.account-sidebar')
 				</div>
-				
+				<div class="col-lg-9">
+					<h3>Welcome back, {{ $firstname }}!</h3>
+					<div class="row">
+						<div class="col-lg-6">
+							<p>Today is {{ date($userdateformat, time()) }}, and the time is {{ date($usertimeformat, time()) }}.</p>
+						</div>
+						<div class="col-lg-6">
+							<p>
+								<strong>Your referral link:</strong><br>
+								<input class="form-control" type="text" name="referrallink" id="referrallink" value="http://{{ Config::get('rtech.appdomain') }}/r/{{ $referral_code }}">
+							</p>
+							<p>You have referred <strong>{{ 0 }}</strong> user(s).</p>
+						</div>
+					</div>
+					
+				</div>
 			</div>
-		</div>
 
+		</div>
 	</div>
 </div>
-
 @stop
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,8 +8,8 @@
 		<form action="{{ route('post.login') }}" method="post" accept-charset="utf-8" role="form" class="form-login">
 			<input type="hidden" name="_token" value="{{ csrf_token() }}">
 			<h1 class="form-login-heading text-muted">Login</h1>
-			<input type="text" class="form-control" value="{{ old('username') }}" placeholder="johndoe123" required="" autofocus="" name="username" id="username">
-			<input type="password" class="form-control" placeholder="supersecretpassword" required="" name="password" id="password">
+			<input type="text" class="form-control" value="{{ old('username') }}" placeholder="username" required="" autofocus="" name="username" id="username">
+			<input type="password" class="form-control" placeholder="password" required="" name="password" id="password">
 			<label>
 				<input type="checkbox" name="remember" id="remember" value="1" {{ old('remember') ? 'checked' : '' }}> Remember me
 			</label>
