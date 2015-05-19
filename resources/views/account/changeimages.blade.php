e52209a85eaca0cd65aa9ecf1738a825688f8ea3
Daniel Billing <daniel@retardedtech.com>
2015-05-19 21:01:07 +0200

    RTUSTWO-4 WIP

--- resources/views/account/changeimages.blade.php
+++ resources/views/account/changeimages.blade.php
@@ -1,10 +1,9 @@
 @extends('layouts.base.main')
 @section('title', 'Change Images')
 @section('content')
-	<div class="container">
+<div class="container">
 	<div class="row">
 		<div class="col-lg-12">
-
 			<h2>Change Images</h2>
 			<hr>
 			<div class="row">
@@ -12,44 +11,68 @@
 					@include('layouts.base.account-sidebar')
 				</div>
 				<div class="col-lg-9">
-					<form action="" method="post" enctype="multipart/form-data">
-						<div class="form-group @if ($errors->has('profilepicture')) has-error @endif">
-							<div class="fileinput fileinput-new" data-provides="fileinput">
-								<div class="fileinput-new thumbnail" style="width: 250px; height: 250px;">
-									<img src="http://placehold.it/250x250" data-src="http://placehold.it/250x250" alt="...">
-								</div>
-								<div class="fileinput-preview fileinput-exists thumbnail" style="height:auto; width:auto; max-width: 250px; max-height: 250px;"></div>
-								<div>
-									<span class="btn-file">
-										<span class="btn btn-primary btn-labeled fileinput-new"><span class="btn-label"><i class="fa fa-picture-o"></i></span>Select image</span>
-										<span class="btn btn-warning btn-labeled fileinput-exists"><span class="btn-label"><i class="fa fa-picture-o"></i></span>Change</span>
-										<input type="file" name="profileimage">
-									</span>
-									<a href="#" class="btn btn-labeled btn-danger fileinput-exists" style="margin-top:12px;" data-dismiss="fileinput"><span class="btn-label"><i class="fa fa-remove"></i></span>Remove</a>
-								</div>
-							</div>
-							@if($errors->has('profilepicture'))
-								<p class="text-danger">{{ $errors->first('profilepicture') }}</p>
-							@endif
-						</div>
-						<hr>
-						<p class="text-right"><button type="submit" class="btn btn-lg btn-labeled btn-success"><span class="btn-label"><i class="fa fa-upload"></i></span>Upload</button></p>
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
-					</form>
-					<!--<img alt="{{$username}}" src="@if($profilepicture){{$profilepicture}}@else{{{'/img/profilepicture/0.png'}}}@endif" class="img-thumbnail">
-					<form action="" method="post" enctype="multipart/form-data">
 
-						<div class="form-group @if ($errors->has('profilepicture')) has-error @endif">
-							<br>
-							<input type="file" name="profilepicture">
-							@if($errors->has('profilepicture'))
-								<p class="text-danger">{{ $errors->first('profilepicture') }}</p>
-							@endif
+					<div role="tabpanel">
+						<ul class="nav nav-tabs" role="tablist">
+							<li role="presentation" class="active"><a href="#profileimage" aria-controls="profileimage" role="tab" data-toggle="tab">Profile Image</a></li>
+							<li role="presentation"><a href="#coverimage" aria-controls="coverimage" role="tab" data-toggle="tab">Cover Image</a></li>
+						</ul>
+						<div class="tab-content">
+							<div role="tabpanel" class="tab-pane active" id="profileimage">
+								<form action="" method="post" enctype="multipart/form-data">
+									<div class="form-group @if ($errors->has('profilepicture')) has-error @endif">
+										<div class="fileinput fileinput-new" data-provides="fileinput">
+											<div class="fileinput-new thumbnail" style="width: 250px; height: 250px;">
+												<img src="{{ $profileimage or 'http://placehold.it/250x250' }}" data-src="http://placehold.it/250x250" alt="...">
+											</div>
+											<div class="fileinput-preview fileinput-exists thumbnail" style="height:auto; width:auto; max-width: 250px; max-height: 250px;"></div>
+											<div>
+												<span class="btn-file">
+													<span class="btn btn-primary btn-labeled fileinput-new"><span class="btn-label"><i class="fa fa-picture-o"></i></span>Select image</span>
+													<span class="btn btn-warning btn-labeled fileinput-exists"><span class="btn-label"><i class="fa fa-picture-o"></i></span>Change</span>
+													<input type="file" name="profileimage">
+												</span>
+												<a href="#" class="btn btn-labeled btn-danger fileinput-exists" style="margin-top:12px;" data-dismiss="fileinput"><span class="btn-label"><i class="fa fa-remove"></i></span>Remove</a>
+											</div>
+										</div>
+										@if($errors->has('profilepicture'))
+											<p class="text-danger">{{ $errors->first('profilepicture') }}</p>
+										@endif
+									</div>
+									<hr>
+									<p class="text-right"><button type="submit" class="btn btn-lg btn-labeled btn-success"><span class="btn-label"><i class="fa fa-upload"></i></span>Upload</button></p>
+									<input type="hidden" name="_token" value="{{ csrf_token() }}">
+								</form>
+							</div>
+							<div role="tabpanel" class="tab-pane" id="coverimage">
+								<form action="" method="post" enctype="multipart/form-data">
+									<div class="form-group @if($errors->has('profilepicture')) has-error @endif">
+										<div class="fileinput fileinput-new" data-provides="fileinput">
+											<div class="fileinput-new thumbnail" style="max-width: 500px; max-height: 160px;">
+												<img src="{{ $profileimage or 'http://placehold.it/1500x480' }}" data-src="http://placehold.it/1500x480" alt="...">
+											</div>
+											<div class="fileinput-preview fileinput-exists thumbnail" style="height:auto; width:auto; max-width: 250px; max-height: 250px;"></div>
+											<div>
+												<span class="btn-file">
+													<span class="btn btn-primary btn-labeled fileinput-new"><span class="btn-label"><i class="fa fa-picture-o"></i></span>Select image</span>
+													<span class="btn btn-warning btn-labeled fileinput-exists"><span class="btn-label"><i class="fa fa-picture-o"></i></span>Change</span>
+													<input type="file" name="profileimage">
+												</span>
+												<a href="#" class="btn btn-labeled btn-danger fileinput-exists" style="margin-top:12px;" data-dismiss="fileinput"><span class="btn-label"><i class="fa fa-remove"></i></span>Remove</a>
+											</div>
+										</div>
+										@if($errors->has('profilepicture'))
+											<p class="text-danger">{{ $errors->first('profilepicture') }}</p>
+										@endif
+									</div>
+									<hr>
+									<p class="text-right"><button type="submit" class="btn btn-lg btn-labeled btn-success"><span class="btn-label"><i class="fa fa-upload"></i></span>Upload</button></p>
+									<input type="hidden" name="_token" value="{{ csrf_token() }}">
+								</form>
+							</div>
 						</div>
-						<hr>
-						<p class="text-right"><button type="submit" class="btn btn-lg btn-labeled btn-warning"><span class="btn-label"><i class="fa fa-picture-o"></i></span>Change</button></p>
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
-					</form>-->
+					</div>
+					
 				</div>
 			</div>
 		</div>
