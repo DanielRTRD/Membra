71003602073d840574bef53a8ce22d6caa2fa773
Daniel Billing <daniel@retardedtech.com>
2015-07-28 15:31:23 +0200

    RTUSTWO-8 WIP

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,32 @@
 @section('title') Home @stop
 
 @section('content')
-<div class="container">
-	<div class="row">
-		<div class="col-md-12">
-			<div class="panel panel-default">
-				<div class="panel-heading">Home</div>
 
-				<div class="panel-body">
-					@if(Auth::guest())
-						You are not logged in.
-					@else
-						You are logged in!
-					@endif
-				</div>
-			</div>
+<section class="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">
+	<div class="mdl-card mdl-cell mdl-cell--12-col">
+		<div class="mdl-card__supporting-text">
+			<h4>Home</h4>
+			@if(Auth::guest())
+				You are not logged in.
+			@else
+				You are logged in!
+			@endif
+		</div>
+		<div class="mdl-card__actions">
+			<a href="#" class="mdl-button">Read our features</a>
 		</div>
 	</div>
-</div>
+	<button data-upgraded=",MaterialButton,MaterialRipple" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon" id="btn3">
+	<i class="material-icons">more_vert</i>
+	<span class="mdl-button__ripple-container"><span class="mdl-ripple"></span></span></button>
+	<div class="mdl-menu__container is-upgraded">
+		<div class="mdl-menu__outline mdl-menu--bottom-right"></div>
+		<ul data-upgraded=",MaterialMenu" class="mdl-menu mdl-js-menu mdl-menu--bottom-right" for="btn3">
+			<li tabindex="-1" class="mdl-menu__item">Lorem</li>
+			<li tabindex="-1" class="mdl-menu__item" disabled="">Ipsum</li>
+			<li tabindex="-1" class="mdl-menu__item">Dolor</li>
+		</ul>
+	</div>
+</section>
+
 @endsection
--- resources/views/layouts/base/main.blade.php
+++ resources/views/layouts/base/main.blade.php
@@ -1,159 +1,192 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="description" content="">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>@yield('title') - {{ Config::get('rtech.appname') }}</title>
 
-	<!-- CSS -->
-	<link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet">
+	<!-- Add to homescreen for Chrome on Android -->
+	<meta name="mobile-web-app-capable" content="yes">
+	<link rel="icon" sizes="192x192" href="img/touch/chrome-touch-icon-192x192.png">
 
-	<!-- Thirdparty CSS -->
-	<link href="{{ asset('/css/jasny-bootstrap.min.css') }}" rel="stylesheet">
-	<link href="{{ asset('/css/font-awesome.min.css') }}" rel="stylesheet">
-	<link href="{{ asset('/css/material-fullpalette.min.css') }}" rel="stylesheet">
-	<link href="{{ asset('/css/ripples.min.css') }}" rel="stylesheet">
-	<link href="{{ asset('/css/roboto.min.css') }}" rel="stylesheet">
+	<!-- Add to homescreen for Safari on iOS -->
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black">
+	<meta name="apple-mobile-web-app-title" content="Material Design Lite">
+	<link rel="apple-touch-icon-precomposed" href="apple-touch-icon-precomposed.png">
 
+	<!-- Tile icon for Win8 (144x144 + tile color) -->
+	<meta name="msapplication-TileImage" content="images/touch/ms-touch-icon-144x144-precomposed.png">
+	<meta name="msapplication-TileColor" content="#3372DF">
+
+	<link href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link rel="stylesheet" href="{{ asset('/css/material.min.css') }}">
+	<link rel="stylesheet" href="https://storage.googleapis.com/code.getmdl.io/1.0.0/material.blue-red.min.css" /> 
 	<!-- Custom CSS -->
+	<link href="{{ asset('/css/material-extra.css') }}" rel="stylesheet">
 	<link href="{{ asset('/css/custom.css') }}" rel="stylesheet">
 	@if(Request::is('login') || Request::is('register'))
 		<link href="{{ asset('/css/login.css') }}" rel="stylesheet">
 	@endif
-
-	<!-- Fonts -->
-	<!--<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>-->
-
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
-
+	<style>
+		#view-source {
+			position: fixed;
+			display: block;
+			right: 0;
+			bottom: 0;
+			margin-right: 40px;
+			margin-bottom: 40px;
+			z-index: 900;
+		}
+	</style>
 </head>
-<body>
-	<nav class="navbar navbar-material-light-blue">
-		<div class="container-fluid">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-					<span class="sr-only">Toggle Navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="{{ url('/') }}">{{ Config::get('rtech.appname') }}</a>
+
+<body class="mdl-demo mdl-color--grey-100 mdl-color-text--grey-700 mdl-base">
+	<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+		<header class="mdl-layout__header mdl-layout__header--scroll mdl-color--primary">
+			<div class="mdl-layout--large-screen-only mdl-layout__header-row"></div>
+			<div class="mdl-layout__header-row">
+				<h3>{{ Config::get('rtech.appname') }}</h3>
 			</div>
-
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				<ul class="nav navbar-nav">
-					<li><a href="{{ url('/') }}">Home</a></li>
-				</ul>
-
-				<ul class="nav navbar-nav navbar-right">
-					@if (Auth::guest())
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Hello, Guest! <span class="caret"></span></a>
-							<ul class="dropdown-menu" role="menu">
-								<li><a href="{{ route('register') }}"><i class="fa fa-pencil"></i> Register</a></li>
-								<li class="divider"></li>
-								<li><a href="{{ route('login') }}"><i class="fa fa-sign-in"></i> Login</a></li>
-							</ul>
-						</li>
-					@else
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Hello, {{ Auth::user()->firstname }}! <span class="caret"></span></a>
-							<ul class="dropdown-menu" role="menu">
-								<li><a href="{{ route('account') }}"><span class="fa fa-user"></span> My Account</a></li>
-								<li class="divider"></li>
-								<li><a href="{{ route('account-change-details') }}"><span class="fa fa-pencil-square-o"></span> Change account details</a></li>
-								<li><a href="{{ route('account-change-password') }}"><span class="fa fa-asterisk"></span> Change password</a></li>
-								<li><a href="{{ route('account-settings') }}"><span class="fa fa-cog"></span> Settings</a></li>
-								<li class="divider"></li>
-								<li><a href="{{ route('logout') }}"><i class="fa fa-sign-out"></i> Logout</a></li>
-							</ul>
-						</li>
-					@endif
-				</ul>
+			<div class="mdl-layout--large-screen-only mdl-layout__header-row">
 			</div>
+			<div class="mdl-layout--large-screen-only mdl-layout__tab-bar mdl-js-ripple-effect mdl-color--primary-dark">
+				<a href="#overview" class="mdl-layout__tab is-active">Overview</a>
+				<a href="#features" class="mdl-layout__tab">Features</a>
+				<a href="#features" class="mdl-layout__tab">Details</a>
+				<a href="#features" class="mdl-layout__tab">Technology</a>
+				<a href="#features" class="mdl-layout__tab">FAQ</a>
+			</div>
+		</header>
+		<div class="mdl-layout__drawer">
+			<span class="mdl-layout-title">{{ Config::get('rtech.appname') }}</span>
+			<nav class="mdl-navigation">
+				<a href="#overview" class="mdl-navigation__link">Overview</a>
+				<a href="#features" class="mdl-navigation__link">Features</a>
+				<a href="#features" class="mdl-navigation__link">Details</a>
+				<a href="#features" class="mdl-navigation__link">Technology</a>
+				<a href="#features" class="mdl-navigation__link">FAQ</a>
+			</nav>
 		</div>
-	</nav>
 
-	@if(count($errors) > 0)
-		<div class="alert alert-danger alert-fixed-top fade in" role="alert">
-			<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-			@if(count($errors) > 1)
-				<p><span class="fa fa-exclamation-circle" aria-hidden="true"></span> <strong>Oh snap!</strong> Something went wrong:</p>
-				<small>
-					<ul>
-						@foreach ($errors->all() as $error)
-							<li>{{ $error }}</li>
-						@endforeach
-					</ul>
-				</small>
-			@else
-				@foreach ($errors->all() as $error)
-					<p><i class="fa fa-exclamation-circle"></i> {{ $error }}</p>
-				@endforeach
-			@endif
-		</div>
-	@endif
-
-	@if(Session::has('message'))
-		@if(Session::has('messagetype'))
-			<div class="alert alert-{{ Session::get('messagetype') }} alert-fixed-top fade in" role="alert">
-		@else
-			<div class="alert alert-info alert-fixed-top fade in" role="alert">
-		@endif
-			<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-			<p>
-				@if(Session::get('messagetype') == 'success')
-					<span class="fa fa-info-circle" aria-hidden="true"></span>
-				@elseif(Session::get('messagetype') == 'warning')
-					<span class="fa fa-exclamation" aria-hidden="true"></span>
-				@elseif(Session::get('messagetype') == 'danger')
-					<span class="fa fa-exclamation-circle" aria-hidden="true"></span>
+		@if(count($errors) > 0)
+			<div class="alert alert-danger alert-fixed-top fade in" role="alert">
+				<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+				@if(count($errors) > 1)
+					<p><span class="fa fa-exclamation-circle" aria-hidden="true"></span> <strong>Oh snap!</strong> Something went wrong:</p>
+					<small>
+						<ul>
+							@foreach ($errors->all() as $error)
+								<li>{{ $error }}</li>
+							@endforeach
+						</ul>
+					</small>
 				@else
-					<span class="fa fa-info-circle" aria-hidden="true"></span>
+					@foreach ($errors->all() as $error)
+						<p><i class="fa fa-exclamation-circle"></i> {{ $error }}</p>
+					@endforeach
 				@endif
-				{{ Session::get('message') }}
-			</p>
-		</div>
-	@endif
-
-	@yield('content')
-
-
-	<div class="footerwrap">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-6 text-muted">
-					<p>&copy; {{ date("Y") }}, <a href="http://rtrdt.ch/" target="_blank">Retarded Tech</a></p>
-					<p class="text-muted"><small>Load time: {{ round((microtime(true) - LARAVEL_START), 3) }}s</small></p>
-				</div>
-				<div class="col-lg-6">
-				    <p class="text-right"><em><a href="http://jira.rtrdt.ch/browse/RTUSTWO?selectedTab=com.atlassian.jira.jira-projects-plugin:changelog-panel" target="_blank">{{Config::get('rtech.appname') . ' ' . Config::get('rtech.appversion') . ' ' . Config::get('rtech.appversiontype') }}</a></em>@if(Config::get('app.debug')) - <b><a href="/resetdb" class="text-danger">DEBUG MODE</a></b> @endif</p>
-				</div>
 			</div>
-		</div>
+		@endif
+
+		@if(Session::has('message'))
+			@if(Session::has('messagetype'))
+				<div class="alert alert-{{ Session::get('messagetype') }} alert-fixed-top fade in" role="alert">
+			@else
+				<div class="alert alert-info alert-fixed-top fade in" role="alert">
+			@endif
+				<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+				<p>
+					@if(Session::get('messagetype') == 'success')
+						<span class="fa fa-info-circle" aria-hidden="true"></span>
+					@elseif(Session::get('messagetype') == 'warning')
+						<span class="fa fa-exclamation" aria-hidden="true"></span>
+					@elseif(Session::get('messagetype') == 'danger')
+						<span class="fa fa-exclamation-circle" aria-hidden="true"></span>
+					@else
+						<span class="fa fa-info-circle" aria-hidden="true"></span>
+					@endif
+					{{ Session::get('message') }}
+				</p>
+			</div>
+		@endif
+		<main class="mdl-layout__content">
+			<div class="mdl-layout__tab-panel is-active" id="overview">
+				@yield('content')
+			</div>
+			<!--<div class="footerwrap">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-6 text-muted">
+							<p>&copy; {{ date("Y") }}, <a href="http://rtrdt.ch/" target="_blank">Retarded Tech</a></p>
+							<p class="text-muted"><small>Load time: {{ round((microtime(true) - LARAVEL_START), 3) }}s</small></p>
+						</div>
+						<div class="col-lg-6">
+							<p class="text-right"><em><a href="http://jira.rtrdt.ch/browse/RTUSTWO?selectedTab=com.atlassian.jira.jira-projects-plugin:changelog-panel" target="_blank">{{Config::get('rtech.appname') . ' ' . Config::get('rtech.appversion') . ' ' . Config::get('rtech.appversiontype') }}</a></em>@if(Config::get('app.debug')) - <b><a href="/resetdb" class="text-danger">DEBUG MODE</a></b> @endif</p>
+						</div>
+					</div>
+				</div>
+			</div>-->
+
+			<footer class="mdl-mega-footer">
+				<div class="mdl-mega-footer--middle-section">
+					<div class="mdl-mega-footer--drop-down-section">
+						<input class="mdl-mega-footer--heading-checkbox" type="checkbox" checked>
+						<h1 class="mdl-mega-footer--heading">Features</h1>
+						<ul class="mdl-mega-footer--link-list">
+							<li><a href="#">About</a></li>
+							<li><a href="#">Terms</a></li>
+							<li><a href="#">Partners</a></li>
+							<li><a href="#">Updates</a></li>
+						</ul>
+					</div>
+					<div class="mdl-mega-footer--drop-down-section">
+						<input class="mdl-mega-footer--heading-checkbox" type="checkbox" checked>
+						<h1 class="mdl-mega-footer--heading">Details</h1>
+						<ul class="mdl-mega-footer--link-list">
+							<li><a href="#">Spec</a></li>
+							<li><a href="#">Tools</a></li>
+							<li><a href="#">Resources</a></li>
+						</ul>
+					</div>
+					<div class="mdl-mega-footer--drop-down-section">
+						<input class="mdl-mega-footer--heading-checkbox" type="checkbox" checked>
+						<h1 class="mdl-mega-footer--heading">Technology</h1>
+						<ul class="mdl-mega-footer--link-list">
+							<li><a href="#">How it works</a></li>
+							<li><a href="#">Patterns</a></li>
+							<li><a href="#">Usage</a></li>
+							<li><a href="#">Products</a></li>
+							<li><a href="#">Contracts</a></li>
+						</ul>
+					</div>
+					<div class="mdl-mega-footer--drop-down-section">
+						<input class="mdl-mega-footer--heading-checkbox" type="checkbox" checked>
+						<h1 class="mdl-mega-footer--heading">FAQ</h1>
+						<ul class="mdl-mega-footer--link-list">
+							<li><a href="#">Questions</a></li>
+							<li><a href="#">Answers</a></li>
+							<li><a href="#">Contact us</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="mdl-mega-footer--bottom-section">
+					<div class="mdl-logo">
+					More Information
+					</div>
+					<ul class="mdl-mega-footer--link-list">
+						<li><a href="https://developers.google.com/web/starter-kit/">Web Starter Kit</a></li>
+						<li><a href="#">Help</a></li>
+						<li><a href="#">Privacy and Terms</a></li>
+					</ul>
+				</div>
+			</footer>
+		</main>
 	</div>
-
-
-	<!-- Scripts -->
-	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-	<script src="{{ asset('/js/bootstrap.min.js') }}"></script>
-	<script src="{{ asset('/js/jasny-bootstrap.min.js') }}"></script>
-
-	<!-- material -->
-	<script src="{{ asset('/js/ripples.min.js') }}"></script>
 	<script src="{{ asset('/js/material.min.js') }}"></script>
-	<script>
-		$(document).ready(function() {
-		   $.material.init();
-		});
-	</script>
-	<!-- /material -->
-
 </body>
 </html>
