35d6104a834d4dff2013e91fe594be79eec0664b
Daniel Billing <daniel@retardedtech.com>
2015-07-29 11:38:20 +0200

    Profile page improved

--- resources/views/account/profile.blade.php
+++ resources/views/account/profile.blade.php
@@ -4,14 +4,24 @@
 @section('content')
 
 <div class="profile">
-	<div class="profile-image-cover" style="background-image: url('{{ $profilecover or '../img/color-splash.jpg' }}');"></div>
+	<div class="profile-image-cover" style="background-image: url('{{ $profilecover or '../img/profilecover/0.jpg' }}');">
+		<div class="visible-xs col-md-12">
+			<img align="left" class="profile-image-xs thumbnail" src="{{ $profilepicture or '../img/profilepicture/0.png' }}" alt="{{ $username }}"/>
+			<div class="profile-text-xs">
+				<h1>{{ $username }} <small>{{ $firstname }} {{ $lastname }}</small></h1>
+				<p>{{ $email }}</p>
+			</div>
+		</div>
+	</div>
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
-				<img align="left" class="profile-image thumbnail" src="{{ $profilepicture or 'http://retardedtech.no/files/img/team/daniel.jpg' }}" alt="{{ $username }}"/>
-				<div class="profile-text">
-					<h1>{{ $firstname }} {{ $lastname }} ({{ $username }})</h1>
-					<p>{{ $email }}</p>
+				<div class="hidden-xs">
+					<img align="left" class="profile-image thumbnail" src="{{ $profilepicture or '../img/profilepicture/0.png' }}" alt="{{ $username }}"/>
+					<div class="profile-text">
+						<h1>{{ $username }} <small>{{ $firstname }} {{ $lastname }}</small></h1>
+						<p>{{ $email }}</p>
+					</div>
 				</div>
 			</div>
 		</div>
