7d2166a119bf6b90d97cde5fc5bb47ae58a4225c
Daniel Billing <daniel@retardedtech.com>
2015-08-02 17:43:29 +0200

    RTUSTWO-5 WIP

--- app/Http/Controllers/Member/AuthController.php
+++ app/Http/Controllers/Member/AuthController.php
@@ -47,9 +47,65 @@ class AuthController extends Controller {
 	}
 
 	public function postRegister(RegisterRequest $request) {
+
 		$user = User::create($request->all());
 		$this->auth->login($user);
 		return Redirect::route('home');
+		
+		/*$validator = Validator::make($request->all(),
+			array(
+				'email' 			=> 'required|max:50|email|unique:users', 
+				'firstname' 		=> 'required|max:250|min:3|regex:/^[A-Za-z0-9\-! ,\'\"\/@\.:\(\)]+$/',
+				'lastname' 			=> 'max:250|min:3|regex:/^[A-Za-z0-9\-! ,\'\"\/@\.:\(\)]+$/',
+				'username' 			=> 'required|max:25|min:3|unique:users|regex:/^[A-Za-z0-9\-! ,\'\"\/@\.:\(\)]+$/|not_in:email', 
+				'password' 			=> 'required|min:6|different:username,firstname',
+				'tos'				=> 'accepted',
+			)
+		);
+
+		if($validator->fails()) {
+			return Redirect::refresh()
+					->withErrors($validator)
+					->withInput();
+		} else {
+			//Create account
+			$email 			= Input::get('email');
+			$firstname 		= Input::get('firstname');
+			$lastname 		= Input::get('lastname');
+			$username 		= Input::get('username');
+			$password 		= Input::get('password');
+			$referral		= Input::get('referral');
+			$referral_code 	= str_random(10);
+
+			//activation code
+			$code 			= str_random(60);
+
+			$user = User::create(array(
+				'email' 		=> $email,
+				'username'		=> $username,
+				'firstname'		=> $firstname,
+				'lastname'		=> $lastname,
+				'password'		=> $password,
+				'code'			=> $code,
+				'referral'		=> $referral,
+				'referral_code'	=> $referral_code,
+				'active'		=> 0
+			));
+
+			if($user) {
+
+				Mail::send('emails.auth.activate', array('link' => URL::route('account-activate', $code), 'firstname' => $firstname), function($message) use ($user) {
+					$message->to($user->email, $user->firstname)->subject('Activate your account');
+				});
+				Session::forget('referral'); //forget the referralRolig
+
+				return Redirect::refresh()
+						->with('globaltype', 'success')
+						->with('global', 'Your account has been created! We have sent you an email to acitvate your account.');
+
+			}
+
+		} */
 	}
 	 
 	public function getLogout() {
