c9f3581ab8e31e7d86cdc4c5dcceecfed338ea7c
Daniel Billing <daniel@retardedtech.com>
2015-12-12 19:56:58 +0100

    Removed subdomains

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -29,219 +29,108 @@ Route::get('/tos', ['as' => 'account-tos-privacy', 'uses' => 'HomeController@ind
 /*
 | IF IN DEBUG MODE THEN DO NOT USE SUBDOMAIN
 */
-if(Config::get('app.debug')) {
+Route::group([
+	'middleware' => 'guest',
+	'prefix' => 'account',
+	], function() {
+		get('/forgotpassword', [
+			'as' => 'account-forgotpassword' ,
+			'uses' => 'Member\PasswordController@getForgotPassword'
+		]);
+		post('/forgotpassword', [
+			'as' => 'account-forgotpassword-post' ,
+			'uses' => 'Member\PasswordController@postForgotPassword'
+		]);
+		get('/recover/{token}', [
+			'as' => 'account-recover' ,
+			'uses' => 'Member\PasswordController@getRecoverAccount'
+		]);
+		post('/recover/{token}', [
+			'as' => 'account-recover-post' ,
+			'uses' => 'Member\PasswordController@postRecoverAccount'
+		]);
+		get('/register', [
+			'as' => 'account-register',
+			'uses' => 'Member\AuthController@getRegister'
+		]);
+		post('/register', [
+			'as' => 'account-register-post',
+			'uses' => 'Member\AuthController@postRegister'
+		]);
+		get('/login', [
+			'as' => 'account-login',
+			'uses' => 'Member\AuthController@getLogin'
+		]);
+		post('/login', [
+			'as' => 'account-login-post',
+			'uses' => 'Member\AuthController@postLogin'
+		]);
+		get('/activate/{activation_code}', [
+			'as' => 'account-activate',
+			'uses' => 'Member\AuthController@getActivate'
+		]);
+});
 
-	Route::group([
-		'middleware' => 'guest',
-		'prefix' => 'account',
-		], function() {
-			get('/forgotpassword', [
-				'as' => 'account-forgotpassword' ,
-				'uses' => 'Member\PasswordController@getForgotPassword'
-			]);
-			post('/forgotpassword', [
-				'as' => 'account-forgotpassword-post' ,
-				'uses' => 'Member\PasswordController@postForgotPassword'
-			]);
-			get('/recover/{token}', [
-				'as' => 'account-recover' ,
-				'uses' => 'Member\PasswordController@getRecoverAccount'
-			]);
-			post('/recover/{token}', [
-				'as' => 'account-recover-post' ,
-				'uses' => 'Member\PasswordController@postRecoverAccount'
-			]);
-			get('/register', [
-				'as' => 'account-register',
-				'uses' => 'Member\AuthController@getRegister'
-			]);
-			post('/register', [
-				'as' => 'account-register-post',
-				'uses' => 'Member\AuthController@postRegister'
-			]);
-			get('/login', [
-				'as' => 'account-login',
-				'uses' => 'Member\AuthController@getLogin'
-			]);
-			post('/login', [
-				'as' => 'account-login-post',
-				'uses' => 'Member\AuthController@postLogin'
-			]);
-			get('/activate/{activation_code}', [
-				'as' => 'account-activate',
-				'uses' => 'Member\AuthController@getActivate'
-			]);
-	});
+Route::group([
+	'middleware' => 'auth',
+	'prefix' => 'user',
+	], function() {
+		get('/', [
+			'as' => 'account' ,
+			'uses' => 'Member\AccountController@index'
+		]);
+		get('/settings', [
+			'as' => 'account-settings' ,
+			'uses' => 'Member\AccountController@getSettings'
+		]);
+		post('/settings', [
+			'as' => 'account-settings-post' ,
+			'uses' => 'Member\AccountController@postSettings'
+		]);
+		get('/change/password', [
+			'as' => 'account-change-password' ,
+			'uses' => 'Member\AccountController@getChangePassword'
+		]);
+		post('/change/password', [
+			'as' => 'account-change-password-post' ,
+			'uses' => 'Member\AccountController@postChangePassword'
+		]);
+		get('/change/details', [
+			'as' => 'account-change-details' ,
+			'uses' => 'Member\AccountController@getChangeDetails'
+		]);
+		post('/change/details', [
+			'as' => 'account-change-details-post' ,
+			'uses' => 'Member\AccountController@postChangeDetails'
+		]);
+		get('/change/images', [
+			'as' => 'account-change-images' ,
+			'uses' => 'Member\AccountController@getChangeImages'
+		]);
+		post('/change/images/profile', [
+			'as' => 'account-change-image-profile-post' ,
+			'uses' => 'Member\AccountController@postChangeProfileImage'
+		]);
+		post('/change/images/cover', [
+			'as' => 'account-change-image-cover-post' ,
+			'uses' => 'Member\AccountController@postChangeProfileCover'
+		]);
+		get('/logout', [
+			'as' => 'logout',
+			'uses' => 'Member\AuthController@getLogout'
+		]);
+});
 
-	Route::group([
-		'middleware' => 'auth',
-		'prefix' => 'user',
-		], function() {
-			get('/', [
-				'as' => 'account' ,
-				'uses' => 'Member\AccountController@index'
-			]);
-			get('/settings', [
-				'as' => 'account-settings' ,
-				'uses' => 'Member\AccountController@getSettings'
-			]);
-			post('/settings', [
-				'as' => 'account-settings-post' ,
-				'uses' => 'Member\AccountController@postSettings'
-			]);
-			get('/change/password', [
-				'as' => 'account-change-password' ,
-				'uses' => 'Member\AccountController@getChangePassword'
-			]);
-			post('/change/password', [
-				'as' => 'account-change-password-post' ,
-				'uses' => 'Member\AccountController@postChangePassword'
-			]);
-			get('/change/details', [
-				'as' => 'account-change-details' ,
-				'uses' => 'Member\AccountController@getChangeDetails'
-			]);
-			post('/change/details', [
-				'as' => 'account-change-details-post' ,
-				'uses' => 'Member\AccountController@postChangeDetails'
-			]);
-			get('/change/images', [
-				'as' => 'account-change-images' ,
-				'uses' => 'Member\AccountController@getChangeImages'
-			]);
-			post('/change/images/profile', [
-				'as' => 'account-change-image-profile-post' ,
-				'uses' => 'Member\AccountController@postChangeProfileImage'
-			]);
-			post('/change/images/cover', [
-				'as' => 'account-change-image-cover-post' ,
-				'uses' => 'Member\AccountController@postChangeProfileCover'
-			]);
-			get('/logout', [
-				'as' => 'logout',
-				'uses' => 'Member\AuthController@getLogout'
-			]);
-	});
-
-	// ADMIN PANEL
-	Route::group([
-		'middleware' => 'auth',
-		'prefix' => 'admin',
-		], function() {
-			/*get('/', [
-				'as' => 'account' ,
-				'uses' => 'Member\AccountController@index'
-			]);*/
-	});
-
-/*
-| ELSE USE DOMAIN INSTEAD
-*/
-
-} else {
-	Route::group([
-		'middleware' => 'guest',
-		'doamin' => 'user.'.Config::get('infihex.appdomain'),
-		], function() {
-			get('/forgotpassword', [
-				'as' => 'account-forgotpassword' ,
-				'uses' => 'Member\PasswordController@getForgotPassword'
-			]);
-			post('/forgotpassword', [
-				'as' => 'account-forgotpassword-post' ,
-				'uses' => 'Member\PasswordController@postForgotPassword'
-			]);
-			get('/recover/{token}', [
-				'as' => 'account-recover' ,
-				'uses' => 'Member\PasswordController@getRecoverAccount'
-			]);
-			post('/recover/{token}', [
-				'as' => 'account-recover-post' ,
-				'uses' => 'Member\PasswordController@postRecoverAccount'
-			]);
-			get('/register', [
-				'as' => 'account-register',
-				'uses' => 'Member\AuthController@getRegister'
-			]);
-			post('/register', [
-				'as' => 'account-register-post',
-				'uses' => 'Member\AuthController@postRegister'
-			]);
-			get('/login', [
-				'as' => 'account-login',
-				'uses' => 'Member\AuthController@getLogin'
-			]);
-			post('/login', [
-				'as' => 'account-login-post',
-				'uses' => 'Member\AuthController@postLogin'
-			]);
-			get('/activate/{activation_code}', [
-				'as' => 'account-activate',
-				'uses' => 'Member\AuthController@getActivate'
-			]);
-	});
-
-	Route::group([
-		'middleware' => 'auth',
-		'domain' => 'user.'.Config::get('infihex.appdomain'),
-		], function() {
-			get('/', [
-				'as' => 'account' ,
-				'uses' => 'Member\AccountController@index'
-			]);
-			get('/settings', [
-				'as' => 'account-settings' ,
-				'uses' => 'Member\AccountController@getSettings'
-			]);
-			post('/settings', [
-				'as' => 'account-settings-post' ,
-				'uses' => 'Member\AccountController@postSettings'
-			]);
-			get('/change/password', [
-				'as' => 'account-change-password' ,
-				'uses' => 'Member\AccountController@getChangePassword'
-			]);
-			post('/change/password', [
-				'as' => 'account-change-password-post' ,
-				'uses' => 'Member\AccountController@postChangePassword'
-			]);
-			get('/change/details', [
-				'as' => 'account-change-details' ,
-				'uses' => 'Member\AccountController@getChangeDetails'
-			]);
-			post('/change/details', [
-				'as' => 'account-change-details-post' ,
-				'uses' => 'Member\AccountController@postChangeDetails'
-			]);
-			get('/change/images', [
-				'as' => 'account-change-images' ,
-				'uses' => 'Member\AccountController@getChangeImages'
-			]);
-			post('/change/images/profile', [
-				'as' => 'account-change-image-profile-post' ,
-				'uses' => 'Member\AccountController@postChangeProfileImage'
-			]);
-			post('/change/images/cover', [
-				'as' => 'account-change-image-cover-post' ,
-				'uses' => 'Member\AccountController@postChangeProfileCover'
-			]);
-			get('/logout', [
-				'as' => 'logout',
-				'uses' => 'Member\AuthController@getLogout'
-			]);
-	});
-
-	// ADMIN PANEL
-	Route::group([
-		'middleware' => 'auth',
-		'domain' => 'admin.'.Config::get('infihex.appdomain'),
-		], function() {
-			/*get('/', [
-				'as' => 'account' ,
-				'uses' => 'Member\AccountController@index'
-			]);*/
-	});
-}
+// ADMIN PANEL
+Route::group([
+	'middleware' => 'auth',
+	'prefix' => 'admin',
+	], function() {
+		/*get('/', [
+			'as' => 'account' ,
+			'uses' => 'Member\AccountController@index'
+		]);*/
+});
 
 Route::get('/user/{username}', ['as' => 'user-profile', 'uses' => 'Member\ProfileController@index']);
 Route::get('/members', ['as' => 'members', 'uses' => 'Member\ProfileController@getMembers']);
