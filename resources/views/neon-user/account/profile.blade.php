19728d48162f21759bc10f97739c35b1274a0f65
Daniel Billing <daniel@retardedtech.com>
2015-12-26 17:52:07 +0100

    MEM-20

--- resources/views/neon-user/account/profile.blade.php
+++ resources/views/neon-user/account/profile.blade.php
@@ -3,28 +3,143 @@
    
 @section('content')
 
-<div class="profile">
-	<div class="profile-image-cover" style="background-image: url('{{ $profilecover or '../img/profilecover/0.jpg' }}');">
-		<div class="visible-xs col-md-12">
-			<img align="left" class="profile-image-xs thumbnail" src="{{ $profilepicture or '../img/profilepicture/0.png' }}" alt="{{ $username }}"/>
-			<div class="profile-text-xs">
-				<h1>{{ $username }} <small>{{ $firstname }} {{ $lastname }}</small></h1>
-				<p>{{ $email }}</p>
-			</div>
+<div class="profile-env">
+	
+	<header class="row">
+		
+		<div class="col-sm-2">
+			
+			<a class="profile-picture" href="{{ route('account-change-images') }}">
+				<img src="{{ $profilepicture or '/images/profilepicture/0.png' }}" class="img-responsive img-circle" />
+			</a>
+			
 		</div>
-	</div>
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12">
-				<div class="hidden-xs">
-					<img align="left" class="profile-image thumbnail" src="{{ $profilepicture or '../img/profilepicture/0.png' }}" alt="{{ $username }}"/>
-					<div class="profile-text">
-						<h1>{{ $username }} <small>{{ $firstname }} {{ $lastname }}</small></h1>
-						<p>{{ $email }}</p>
+		
+		<div class="col-sm-7">
+			
+			<ul class="profile-info-sections">
+				<li>
+					<div class="profile-name">
+						<strong>
+							{{ $firstname }}@if($showname) {{ $lastname }}@endif
+							<a href="#" class="user-status is-offline tooltip-primary" data-toggle="tooltip" data-placement="top" data-original-title="Offline"></a>
+							<!-- User statuses available classes "is-online", "is-offline", "is-idle", "is-busy" -->						</strong>
+						<span>
+							@if($ismod)
+								Moderator
+							@elseif($isadmin)
+								Administrator
+							@elseif($issuperadmin)
+								Administrator
+							@else
+								Member
+							@endif							
+						</span>
 					</div>
-				</div>
-			</div>
+				</li>
+				
+				
+				<li>
+					<div class="profile-stat">
+						<h3>0</h3>
+						<span><a href="#">seats reserved</a></span>
+					</div>
+				</li>
+				<!--
+				<li>
+					<div class="profile-stat">
+						<h3>0</h3>
+						<span><a href="#">following</a></span>
+					</div>
+				</li>
+				-->
+			</ul>
+			
 		</div>
-	</div>
-</div> 
+		
+		<div class="col-sm-3">
+			<!--
+			<div class="profile-buttons">
+				<a href="#" class="btn btn-default">
+					<i class="fa fa-envelope-o"></i>
+				</a>
+			</div>
+			-->
+		</div>
+		
+	</header>
+	
+	<section class="profile-info-tabs">
+		
+		<div class="row">
+			
+			<div class="col-sm-offset-2 col-sm-10">
+				
+				<ul class="user-details">
+					
+					@if($location)
+						<li>
+							<a href="#">
+								<i class="fa fa-location"></i>
+								{{ $location }}
+							</a>
+						</li>
+					@endif
+
+					@if($gender)
+						<li>
+							<a href="#">
+								<i class="fa fa-genderless"></i>
+								{{ $gender }}
+							</a>
+						</li>
+					@endif
+
+					@if($occupation)
+						<li>
+							<a href="#">
+								<i class="fa fa-suitcase"></i>
+								{{ $occupation }}
+							</a>
+						</li>
+					@endif
+
+					@if($showemail)
+						<li>
+							<a href="#">
+								<i class="fa fa-mail"></i>
+								{{ $email }}
+							</a>
+						</li>
+					@endif
+					
+					<li>
+						<a href="#">
+							<i class="fa fa-calendar"></i>
+							{{ $birthdate }}
+						</a>
+					</li>
+				</ul>
+				
+				
+				<!-- tabs for the profile links -->
+				<ul class="nav nav-tabs">
+					<li class="active"><a href="#profile-info">Profile</a></li>
+					@if(\Auth::user()->id == $id)
+						<li><a href="{{ route('account-change-details') }}"><i class="fa fa-edit"></i> Edit Profile Details</a></li>
+						<li><a href="{{ route('account-change-password') }}"><i class="fa fa-asterisk"></i> Change Password</a></li>
+						<li><a href="{{ route('account-settings') }}"><i class="fa fa-cog"></i> Edit Profile Settings</a></li>
+					@endif
+				</ul>
+				
+			</div>
+			
+		</div>
+		
+	</section>
+
+</div>
+
+Moar to come.<br><br><br><br>
+
 @stop
