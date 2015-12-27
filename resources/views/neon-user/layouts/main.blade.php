eb06db29d09afd13f04526fe4bf087e512fe8773
Daniel Billing <daniel@retardedtech.com>
2015-12-27 20:03:09 +0100

    Added loadtime

--- resources/views/neon-user/layouts/main.blade.php
+++ resources/views/neon-user/layouts/main.blade.php
@@ -96,6 +96,7 @@
 					<div class="row">
 						<div class="col-md-6">
 							<p>&copy; {{ date('Y') }}, Infihex</p>
+							<p class="text-muted"><small>Load time: {{ round((microtime(true) - LARAVEL_START), 3) }}s</small></p>
 						</div>
 						<div class="col-md-6 text-right">
 							<p>
