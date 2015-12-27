31c5b29fdddfd0fcb0cd95700c32b080776ed8c2
Daniel Billing <daniel@retardedtech.com>
2015-12-27 20:04:14 +0100

    Template changes

--- resources/views/neon/layouts/main.blade.php
+++ resources/views/neon/layouts/main.blade.php
@@ -40,7 +40,6 @@
 				<nav class="site-nav">
 					<ul class="main-menu hidden-xs" id="main-menu">
 						<li class="active"><a href="{{ url('/') }}"><span>Home</span></a></li>
-						<li><a href="{{ route('members') }}"><span><i class="fa fa-users"></i> Members</span></a></li>
 						@if(Auth::Guest())
 							<li><a href="{{ route('account-login') }}"><span>Login</span></a></li>
 						@else
@@ -55,97 +54,6 @@
 		</div>
 	</div>
 </div>
-
-<!--
-	<nav class="navbar navbar-material-light-blue">
-		<div class="container-fluid">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-					<span class="sr-only">Toggle Navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="{{ url('/') }}">{{ Config::get('infihex.appname') }}</a>
-			</div>
-
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				<ul class="nav navbar-nav">
-					<li><a href="{{ url('/') }}"><i class="fa fa-home"></i> Home</a></li>
-					<li><a href="{{ route('members') }}"><i class="fa fa-users"></i> Members</a></li>
-				</ul>
-
-				<ul class="nav navbar-nav navbar-right">
-					@if (Auth::guest())
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Hello, Guest! <span class="caret"></span></a>
-							<ul class="dropdown-menu" role="menu">
-								<li><a href="{{ route('account-register') }}"><i class="fa fa-pencil"></i> Register</a></li>
-								<li class="divider"></li>
-								<li><a href="{{ route('account-login') }}"><i class="fa fa-sign-in"></i> Login</a></li>
-							</ul>
-						</li>
-					@else
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Hello, {{ Auth::user()->firstname }}! <span class="caret"></span></a>
-							<ul class="dropdown-menu" role="menu">
-								<li><a href="{{ route('account') }}"><span class="fa fa-user"></span> My Account</a></li>
-								<li class="divider"></li>
-								<li><a href="{{ route('account-change-details') }}"><span class="fa fa-pencil-square-o"></span> Change account details</a></li>
-								<li><a href="{{ route('account-change-password') }}"><span class="fa fa-asterisk"></span> Change password</a></li>
-								<li><a href="{{ route('account-settings') }}"><span class="fa fa-cog"></span> Settings</a></li>
-								<li class="divider"></li>
-								<li><a href="{{ route('logout') }}"><i class="fa fa-sign-out"></i> Logout</a></li>
-							</ul>
-						</li>
-					@endif
-				</ul>
-			</div>
-		</div>
-	</nav>
-
-	@if(count($errors) > 0)
-		<div class="alert alert-danger alert-fixed-top fade in" role="alert">
-			<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-			@if(count($errors) > 1)
-				<p><span class="fa fa-exclamation-circle" aria-hidden="true"></span> <strong>Oh snap!</strong> Something went wrong:</p>
-				<small>
-					<ul>
-						@foreach ($errors->all() as $error)
-							<li>{{ $error }}</li>
-						@endforeach
-					</ul>
-				</small>
-			@else
-				@foreach ($errors->all() as $error)
-					<p><i class="fa fa-exclamation-circle"></i> {{ $error }}</p>
-				@endforeach
-			@endif
-		</div>
-	@endif
-
-	@if(Session::has('message'))
-		@if(Session::has('messagetype'))
-			<div class="alert alert-{{ Session::get('messagetype') }} alert-fixed-top fade in" role="alert">
-		@else
-			<div class="alert alert-info alert-fixed-top fade in" role="alert">
-		@endif
-			<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-			<p>
-				@if(Session::get('messagetype') == 'success')
-					<span class="fa fa-info-circle" aria-hidden="true"></span>
-				@elseif(Session::get('messagetype') == 'warning')
-					<span class="fa fa-exclamation" aria-hidden="true"></span>
-				@elseif(Session::get('messagetype') == 'danger')
-					<span class="fa fa-exclamation-circle" aria-hidden="true"></span>
-				@else
-					<span class="fa fa-info-circle" aria-hidden="true"></span>
-				@endif
-				{{ Session::get('message') }}
-			</p>
-		</div>
-	@endif
--->
 	
 @yield('content')
 
@@ -153,17 +61,15 @@
 <footer class="site-footer">
 	<div class="container">
 		<div class="row">
-			<div class="col-sm-6">
-				<p>&copy; {{ date("Y") }}, <a href="https://infihex.com/" target="_blank">Infihex</a></p>
+			<div class="col-md-6">
+				<p>&copy; {{ date('Y') }}, Infihex</p>
 				<p class="text-muted"><small>Load time: {{ round((microtime(true) - LARAVEL_START), 3) }}s</small></p>
 			</div>
-			<div class="col-sm-6">
-				<p>
-					<small>
-						<a href="http://jira.infihex.com/projects/MEM/issues" target="_blank">{{Config::get('infihex.appname') . ' ' . Config::get('infihex.appversion') . ' ' . Config::get('infihex.appversiontype') }}</a>
-						@if(Config::get('app.debug')) - <b><a href="/resetdb" class="text-danger">DEBUG MODE</a></b> @endif
-					</small>
-				</p>
+			<div class="col-md-6 text-right">
+				<p><small>
+					<a href="http://jira.infihex.com/projects/MEM/issues" target="_blank">{{Config::get('infihex.appname') . ' ' . Config::get('infihex.appversion') . ' ' . Config::get('infihex.appversiontype') }}</a>
+					@if(Config::get('app.debug')) <b>(<a href="/resetdb" class="text-danger">DEBUG MODE</a>)</b> @endif by <a href="https://infihex.com/" target="_blank">Infihex</a>
+				</small></p>
 			</div>
 		</div>
 	</div>
