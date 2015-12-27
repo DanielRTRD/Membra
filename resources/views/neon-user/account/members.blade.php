6fc876cfd831ca152e5e3f6af7d4ed0fe81a537f
Daniel Billing <daniel@retardedtech.com>
2015-12-27 01:09:07 +0100

    MEM-23

--- resources/views/neon-user/account/members.blade.php
+++ resources/views/neon-user/account/members.blade.php
@@ -5,7 +5,7 @@
 <div class="container">
 	<div class="row">
 		<div class="col-md-9 col-sm-7">
-			<h2>Members</h2>
+			<h1>Members</h1>
 		</div>
 		
 		<div class="col-md-3 col-sm-5">
@@ -22,47 +22,118 @@
 		</div>
 
 	</div>
-<hr>
-	@foreach($members as $member)
-		<div class="member-entry">
-			<a href="{{ route('user-profile', $member->username) }}" class="member-img">
-				<img src="{{ $member->profilepicture or '/images/profilepicture/0.png' }}" class="img-rounded" />
-				<i class="fa fa-share" style="text-shadow:#000 0 0 10px"></i>
-			</a>
-			<div class="member-details">
-				<h4>
-					<a href="{{ route('user-profile', $member->username) }}">{{ $member->firstname }}@if($member->showname) {{ $member->lastname }}@endif</a>
-				</h4>
-				<div class="row info-list">
-					<div class="col-sm-4">
-						<i class="fa fa-briefcase"></i> {{ $member->occupation }}
-					</div>
-					<div class="col-sm-4">
-						<i class="fa fa-twitter"></i>
-					</div>
-					<div class="col-sm-4">
-						<i class="fa fa-facebook"></i>
-					</div>
-					
-					<div class="clear"></div>
 
-					<div class="col-sm-4">
-						<i class="fa fa-map-marker"></i> {{ $member->location or '<em>Unkown</em>' }}
+	<div class="row">
+		<div class="col-md-8">
+			@foreach($members as $member)
+				<div class="member-entry">
+					<a href="{{ route('user-profile', $member->username) }}" class="member-img">
+						<img src="{{ $member->profilepicture or '/images/profilepicture/0.png' }}" class="img-rounded" />
+						<i class="fa fa-share" style="text-shadow:#000 0 0 10px"></i>
+					</a>
+					<div class="member-details">
+						<h4>
+							<a href="{{ route('user-profile', $member->username) }}">{{ $member->firstname }}@if($member->showname) {{ $member->lastname }}@endif</a>
+						</h4>
+						<div class="row info-list">
+							<div class="col-sm-4">
+								<i class="fa fa-briefcase"></i> {{ $member->occupation }}
+							</div>
+							<div class="col-sm-4">
+								<i class="fa fa-twitter"></i>
+							</div>
+							<div class="col-sm-4">
+								<i class="fa fa-facebook"></i>
+							</div>
+							
+							<div class="clear"></div>
+
+							<div class="col-sm-4">
+								<i class="fa fa-map-marker"></i> {{ $member->location or '<em>Unkown</em>' }}
+							</div>
+							<div class="col-sm-4">
+								<i class="fa fa-genderless"></i> {{ $member->gender }}
+							</div>
+							<div class="col-sm-4">
+								<i class="fa fa-birthday-cake"></i> {{ date_diff(date_create($member->birthdate), date_create('today'))->y }}
+							</div>
+						</div>
 					</div>
-					<div class="col-sm-4">
-						<i class="fa fa-genderless"></i> {{ $member->gender }}
-					</div>
-					<div class="col-sm-4">
-						<i class="fa fa-birthday-cake"></i> {{ date_diff(date_create($member->birthdate), date_create('today'))->y }}
-					</div>
+				</div>
+			@endforeach
+
+			<div class="row">
+				<div class="col-md-12">
+					{{ $members->render() }}
 				</div>
 			</div>
 		</div>
-	@endforeach
+		<div class="col-md-4">
+			<h3>Newest Members</h3>
+			<hr>
+			@foreach($newestmembers as $member)
+				<div class="member-entry">
+					<a href="{{ route('user-profile', $member->username) }}" class="member-img">
+						<img src="{{ $member->profilepicture or '/images/profilepicture/0.png' }}" class="img-rounded" />
+						<i class="fa fa-share" style="text-shadow:#000 0 0 10px"></i>
+					</a>
+					<div class="member-details">
+						<h4>
+							<a href="{{ route('user-profile', $member->username) }}">{{ $member->firstname }}@if($member->showname) {{ $member->lastname }}@endif</a>
+						</h4>
+						<div class="row info-list">
+							<div class="col-sm-6">
+								<i class="fa fa-briefcase"></i> {{ $member->occupation }}
+							</div>
+							<div class="col-sm-6">
+								<i class="fa fa-map-marker"></i> {{ $member->location or '<em>Unkown</em>' }}
+							</div>
+							
+							<div class="clear"></div>
+							
+							<div class="col-sm-6">
+								<i class="fa fa-genderless"></i> {{ $member->gender }}
+							</div>
+							<div class="col-sm-6">
+								<i class="fa fa-birthday-cake"></i> {{ date_diff(date_create($member->birthdate), date_create('today'))->y }}
+							</div>
+						</div>
+					</div>
+				</div>
+			@endforeach
 
-	<div class="row">
-		<div class="col-md-12">
-			{{ $members->render() }}
+			<h3>Last Online Members</h3>
+			<hr>
+			@foreach($onlinemembers as $member)
+				<div class="member-entry">
+					<a href="{{ route('user-profile', $member->username) }}" class="member-img">
+						<img src="{{ $member->profilepicture or '/images/profilepicture/0.png' }}" class="img-rounded" />
+						<i class="fa fa-share" style="text-shadow:#000 0 0 10px"></i>
+					</a>
+					<div class="member-details">
+						<h4>
+							<a href="{{ route('user-profile', $member->username) }}">{{ $member->firstname }}@if($member->showname) {{ $member->lastname }}@endif</a>
+						</h4>
+						<div class="row info-list">
+							<div class="col-sm-6">
+								<i class="fa fa-briefcase"></i> {{ $member->occupation }}
+							</div>
+							<div class="col-sm-6">
+								<i class="fa fa-map-marker"></i> {{ $member->location or '<em>Unkown</em>' }}
+							</div>
+							
+							<div class="clear"></div>
+							
+							<div class="col-sm-6">
+								<i class="fa fa-genderless"></i> {{ $member->gender }}
+							</div>
+							<div class="col-sm-6">
+								<i class="fa fa-birthday-cake"></i> {{ date_diff(date_create($member->birthdate), date_create('today'))->y }}
+							</div>
+						</div>
+					</div>
+				</div>
+			@endforeach
 		</div>
 	</div>
 
